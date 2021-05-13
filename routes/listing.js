09a38e1f83e45364cdd6548c5585f0c47721e3a2
mattwilliams150 <matthewdavidwilliams@me.com>
2021-05-13 22:34:12 +0100

    add tags to reviews

--- routes/listing.js
+++ routes/listing.js
@@ -1,6 +1,8 @@
 var Review = require('../models/review');
 var Places = require('../models/places');
 var gdata = require('../models/googledata');
+var categories = require("../config/categories.json");
+
 
 module.exports = (app) => {
     app.get('/listing', (req, res) => {
@@ -39,6 +41,7 @@ module.exports = (app) => {
                     placeid: placeid,
                     mongoplace: mongoplace[0].data.result,
                     place: place,
+                    categories: categories,
                     loggedIn: loggedIn,
                     reviews: review,
                     clientPlacesApiKey: clientPlacesApiKey
@@ -51,11 +54,42 @@ module.exports = (app) => {
     app.post('/listingreview', (req, res) => {
         var newReview = new Review();
         newReview.placeid = req.query.placeid
-        newReview.name = req.body.review_name;
+        newReview.name = req.body.review_name
         newReview.email = req.body.review_email
         newReview.rating = req.body.review_rating
         newReview.review = req.body.review_review
         newReview.date = Date.now()
+        newReview.arty = req.body.arty
+        newReview.design = req.body.design
+        newReview.authentic = req.body.authentic
+        newReview.brunch = req.body.brunch
+        newReview.cocktails = req.body.cocktails
+        newReview.food = req.body.food
+        newReview.dogs = req.body.dogs
+        newReview.elegant = req.body.elegant
+        newReview.entertaining = req.body.entertaining
+        newReview.beer = req.body.beer
+        newReview.family = req.body.family
+        newReview.service = req.body.service
+        newReview.wines = req.body.wines
+        newReview.portions = req.body.portions
+        newReview.glamorous = req.body.glamorous
+        newReview.groups = req.body.groups
+        newReview.atmosphere = req.body.atmosphere
+        newReview.meetings = req.body.meetings
+        newReview.music = req.body.music
+        newReview.healthy = req.body.healthy
+        newReview.intimate = req.body.intimate
+        newReview.lgbtq = req.body.lgbtq
+        newReview.modern = req.body.modern
+        newReview.party = req.body.party
+        newReview.popup = req.body.popup
+        newReview.quirky = req.body.quirky
+        newReview.relaxed = req.body.relaxed
+        newReview.romantic = req.body.romantic
+        newReview.rustic = req.body.rustic
+        newReview.vegan = req.body.vegan
+        newReview.vegetarian = req.body.vegetarian
 
         newReview.save((err) => {
             console.log(err);
