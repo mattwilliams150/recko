81d40bf60072da7c5fcfd235b47dde98b0d282d0
mattwilliams150 <matthewdavidwilliams@me.com>
2021-11-15 18:31:33 +0000

    clean up static tags when submitting review
    changed static tags to use dynamic tags from categories.tagObj

--- routes/listing.js
+++ routes/listing.js
@@ -98,41 +98,9 @@ module.exports = (app) => {
         newReview.rating = req.body.review_rating
         newReview.review = req.body.review_review
         newReview.date = Date.now()
-        newReview.culturally_authentic_dishes = req.body.culturally_authentic_dishes
-        newReview.exciting_menu = req.body.exciting_menu
-        newReview.fresh_flavours = req.body.fresh_flavours
-        newReview.generous_portions = req.body.generous_portions
-        newReview.fine_dining = req.body.fine_dining
-        newReview.small_plates_sharing = req.body.small_plates_sharing
-        newReview.street_food_vibes = req.body.street_food_vibes
-        newReview.delicious_cocktails = req.body.delicious_cocktails
-        newReview.craft_local_beer = req.body.craft_local_beer
-        newReview.fantastic_wine_list = req.body.fantastic_wine_list
-        newReview.awesome_architecture = req.body.awesome_architecture
-        newReview.entertaining = req.body.entertaining
-        newReview.fantastic_service = req.body.fantastic_service
-        newReview.luxurious = req.body.luxurious
-        newReview.buzzing_atmosphere = req.body.buzzing_atmosphere
-        newReview.great_music = req.body.great_music
-        newReview.intimate = req.body.intimate
-        newReview.modern_design = req.body.modern_design
-        newReview.party_vibes = req.body.party_vibes
-        newReview.quirky = req.body.quirky
-        newReview.relaxing = req.body.relaxing
-        newReview.romantic = req.body.romantic
-        newReview.rustic = req.body.rustic
-        newReview.chic = req.body.chic
-        newReview.working_from_home = req.body.working_from_home
-        newReview.young_kids_families = req.body.young_kids_families
-        newReview.large_groups = req.body.large_groups
-        newReview.vegans_vegetarians = req.body.vegans_vegetarians
-        newReview.bringing_the_dog = req.body.bringing_the_dog
-        newReview.a_date = req.body.a_date
-        newReview.a_special_occasion = req.body.a_special_occasion
-        newReview.a_quiet_catch_up = req.body.a_quiet_catch_up
-        newReview.watching_sport = req.body.watching_sport
-        newReview.an_evening_with_friends = req.body.an_evening_with_friends
-        newReview.bottomless_brunch = req.body.bottomless_brunch
+        for (tag in categories.tagObj) {
+            newReview[tag] = req.body[tag];
+        };
 
         newReview.save((err) => {
             console.log(err);
